ecedb1d2f666bdfcbc6ba2c06b71e55455f513b9
hotlatteiceamericano <hotlatteiceamericano@gmail.com>
2026-03-24 14:57:43 +0800

    add mongodb feature dependency

--- src/user.rs
+++ src/user.rs
@@ -1,15 +1,26 @@
 use ratatui::{text::Line, widgets::ListItem};
+use serde::{Deserialize, Serialize};
 
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct User {
+    #[cfg_attr(
+        feature = "mongodb",
+        serde(
+            serialize_with = "bson::serde_helpers::serialize_object_id_as_hex_string",
+            rename = "_id",
+        )
+    )]
     id: u32,
     display_name: String,
+    email: String,
 }
 
 impl User {
-    pub fn new(id: u32, display_name: &str) -> Self {
+    pub fn new(id: u32, display_name: &str, email: String) -> Self {
         Self {
             id,
             display_name: display_name.to_string(),
+            email,
         }
     }
 

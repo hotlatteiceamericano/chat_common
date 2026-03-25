9773043127779606fd348c68b464ee73efed69f2
hotlatteiceamericano <hotlatteiceamericano@gmail.com>
2026-03-25 13:40:35 +0800

    update User.id to be an Optional field since MongoDB is supposed to create it
    Also, the id() getter method returns Option<&str> instead of
    &Option<String>. It is important to remember when returning as
    &Option<String>, the inner String value is actually OWNED, which is what
    we don't want. Hence we change to Option<&str> convert the innver value
    to be a reference but not creating reference on the "Option> part.

--- src/user.rs
+++ src/user.rs
@@ -3,29 +3,24 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct User {
-    #[cfg_attr(
-        feature = "mongodb",
-        serde(
-            serialize_with = "bson::serde_helpers::serialize_object_id_as_hex_string",
-            rename = "_id",
-        )
-    )]
-    id: u32,
+    #[cfg_attr(feature = "mongodb", serde(rename = "_id",))]
+    #[serde(skip_serializing_if = "Option::is_none", default)]
+    id: Option<String>,
     display_name: String,
     email: String,
 }
 
 impl User {
-    pub fn new(id: u32, display_name: &str, email: String) -> Self {
+    pub fn new(display_name: &str, email: String) -> Self {
         Self {
-            id,
+            id: None,
             display_name: display_name.to_string(),
             email,
         }
     }
 
-    pub fn id(&self) -> u32 {
-        self.id
+    pub fn id(&self) -> Option<&str> {
+        self.id.as_deref()
     }
     pub fn display_name(&self) -> &str {
         self.display_name.as_str()
@@ -37,7 +32,7 @@ impl<'a> From<&'a User> for ListItem<'a> {
         ListItem::new(Line::from(format!(
             "{}({})",
             value.display_name(),
-            value.id()
+            value.id().unwrap_or("")
         )))
     }
 }
